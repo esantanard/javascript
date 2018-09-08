4bf43c53e303f0ae8cb1c10832ebb2508f91aadd
Eduardo Santana <esantanard@hotmail.com>
2018-09-07 23:37:34 -0400

    escribir signos

--- js/app.js
+++ js/app.js
@@ -10,17 +10,55 @@ function volverSize(elemento){
 function escribirPantalla(elemento){
 	elemento=elemento.alt
 	var pant=document.getElementById("display").innerHTML
-	if(pant==0){
-		document.getElementById("display").innerHTML="";
-		document.getElementById("display").innerHTML+=elemento;	
-	}else if(pant!==0){
-		document.getElementById("display").innerHTML+=elemento;	
-	}
-	else{
-		  document.getElementById("display").innerHTML=0;
+	//Aqui estoy tratando de que si lo que hay en el display es mayor a 0 y menor que uno se visualice el decimal
+	if(pant=="0"){
+		document.getElementById("display").innerHTML=elemento
+	}else{
+		if(elemento== "mas"){
+			elemento= "+"
+			document.getElementById("display").innerHTML+=elemento;	
+		}else{
+			if(elemento== "menos"){
+				elemento= "-"
+				document.getElementById("display").innerHTML+=elemento;	
+			}else{
+				if(elemento== "por"){
+					elemento= "*"
+					document.getElementById("display").innerHTML+=elemento;	
+				}else{
+					if(elemento== "dividido"){
+						elemento= "/"
+						document.getElementById("display").innerHTML+=elemento;	
+					}else{
+						if(elemento=="raiz"){
+
+						}else{
+							document.getElementById("display").innerHTML+=elemento
+						}
+					}
+				}	
+			}
+		}
 	}
 }
 
+//Para hacer que la calculadora realice su funcion de calcular
+function calcular(opcion) {
+	var resultado = 0;
+	switch (opcion) {
+		case "mas":
+		resultado = parseFloat(elemento1) + parseFloat(elemento2)
+		break;
+
+		case "-":
+		resultado = parseFloat(elemento1) - parseFloat(elemento2)
+		break;
+
+		case "igual":
+		console.log(resultado);
+		break;
+	}
+}
 
 var Calculadora = {
 	init : function(){
@@ -28,6 +66,8 @@ var Calculadora = {
 		this.pantalla();
 		document.getElementById("on").onclick=this.presionarON;
 		document.getElementById("sign").onclick=this.agregarNegativo;
+		document.getElementById("punto").onclick=this.teclaPunto;
+		this.asignarEventCalcular();
 	},
 	asignarEventoTeclado : function(){
 		var tecl1 = document.querySelectorAll(".teclado img")
@@ -61,6 +101,24 @@ var Calculadora = {
 		else{
 			document.getElementById("display").insertAdjacentHTML("afterbegin", "-");
 		}
+	},
+	teclaPunto : function(){
+		var panta = document.getElementById("display").innerHTML
+		if(panta.indexOf(".")!=1){
+			document.getElementById("display").insertAdjacentHTML("beforeend", ".");
+		}
+		else{
+			
+		}
+	},
+	asignarEventCalcular: function(){
+		var rr = document.querySelectorAll(".teclas")
+		for (var i = 0; i < rr.length; i++) {
+			rr[i].onclick=this.eventCalcular;			
+		}	
+	},
+	eventCalcular:function(e){
+		calcular(e.target)
 	}
 }
 
